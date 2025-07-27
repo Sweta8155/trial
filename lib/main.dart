4a4091f51b5903a8a4bfebed3f31baa760a9f6b9
Sweta Badgujar <swetabadgujar1234@gmail.com>
2025-07-27 11:49:57 +0530

    Created project from initially by passing parameter

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:git_trial/screens/home_screen.dart';
+void main() {
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter Demo Home Page'),
+        ),
+        body: HomeScreen(title: "Home screen parameter",),
+      ),
+    );
+  }
+}
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+class HomeScreen extends StatelessWidget {
+  final String title;
+  const HomeScreen({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("${title}"),
+      ),
+      body: Center(
+        child: Text(
+          'Welcome to the Home Screen!',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
